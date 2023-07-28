05122b52de871dc2a7dae69f8edaf0876e19e121
merlin-04 <merlinkuriakose9@gmail.com>
2023-07-28 07:26:03 +0530

    Modified AreaMethod File

--- AreaMethod.java
+++ AreaMethod.java
@@ -3,12 +3,12 @@ class AreaMethod
 	public static void area(float radius)
 	{
 		float area = 3.142f * radius*radius;
-		System.out.println("Area of the circle is "+area);
+		System.out.println("Area of the circle is  "+area);
 	}
 	public static void area(int width,int height)
 	{
 		int area = width*height;
-		System.out.println("Area of the rectangle is "+area);
+		System.out.println("Area of the rectangle is  "+area);
 	}
 	public static void area(int side)
 	{
